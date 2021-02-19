a89432bb29440f8efe3965bed6fce1b1cae314a5
AdrienG259 <AdrienG259@users.noreply.github.com>
2021-02-19 14:42:02 +0100

    Revert "Delete src directory"
    This reverts commit f918414ae8aa38802d94fe71835f8f216e518897.

---
+++ src/sample/Controleur.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Controleur {
+
+}
+
---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.lang.Integer;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("acceuil.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(new Controleur());
+        primaryStage.setTitle("EnstarDesktop");
+        Scene accueil = new Scene(root, 640, 360);
+        primaryStage.setScene(accueil);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
