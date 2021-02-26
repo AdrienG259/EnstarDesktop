7f84bf0431028a42ef55f264b05c631e5abb6e28
Emilie <emilie.simon97@gmail.com>
2021-02-26 09:59:29 +0100

    3 classes protocole pour 3 cas de figure

---
+++ src/server/ProtocoleGroupe.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.net.Socket;
+
+public class ProtocoleGroupe implements IProtocole{
+    @Override
+    public void execute(TCPServer unServeur, Socket clientSocket) {
+
+    }
+}
