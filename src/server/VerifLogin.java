f883a3bf8048c93eb9b3cd3a0e69ec6331cd9906
Marie-Amelie-Defresne <marie-amelie.defresne@ensta-bretagne.org>
2021-02-27 19:04:52 +0100

    Ajout de la lecture des informations de connexion

---
+++ src/server/VerifLogin.java
@@ -0,0 +1,19 @@
+package server;
+
+import java.io.File;
+
+public class VerifLogin {
+
+    private final String identifiant;
+    private final String motdepasse;
+    private final File fichierID = new File("src/server/FichierID.txt");
+
+    public VerifLogin(String login, String mdp) {
+        this.identifiant = login;
+        this.motdepasse = mdp;
+    }
+
+    public boolean comparaison(String identifiant, String motdepasse) {
+
+    }
+}
