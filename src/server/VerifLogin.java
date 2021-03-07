5507db7b11df240158d1459bc276c668935b0e2c
Emilie <emilie.simon97@gmail.com>
2021-03-07 16:49:50 +0100

    suppression du fichier texte avec les identifiants

--- src/server/VerifLogin.java
+++ src/server/VerifLogin.java
@@ -6,7 +6,6 @@ public class VerifLogin {
 
     private final String identifiant;
     private final String motdepasse;
-    private final File fichierID = new File("src/server/FichierID.txt");
 
     AutorizedUser autorizedUser;
 
