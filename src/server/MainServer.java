e9deda4f83fb62f060fabe35764f3dfd6dbcfe16
Emilie <emilie.simon97@gmail.com>
2021-03-16 19:59:48 +0100

    ajout de la classe Messagerie et de l'interface IMessagerie pour pouvoir ouvrir les serveurs

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -9,8 +9,13 @@ import java.util.ArrayList;
 public class MainServer {
 
 	public static void main( String[] args) {
-		ServeurTCP aServer = new ServeurTCP((IContext) new ConversationSimple("toto", new ArrayList<User>()),new ProtocoleServeurPrivee(),6666 );
-		aServer.start();
+
+		Messagerie maMesssagerie = new Messagerie(); // je pense qu'il faudra lui rajouter des arguments
+		System.out.println("ouverture de la messagerie");
+		maMesssagerie.ouvrirMessagerie();
+
+		//ServeurTCP aServer = new ServeurTCP((IContext) new ConversationSimple("toto", new ArrayList<User>()),new ProtocoleServeurPrivee(),6666 );
+		//aServer.start();
 		
 		
 	}
--- src/server/Messagerie.java
+++ src/server/Messagerie.java
@@ -1,8 +1,30 @@
 package server;
 
-public class Messagerie implements IContext, IMessagerie{
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class Messagerie extends Observable implements IContext, IMessagerie{
 
     // équivalent de la classe Banque du TD3 pour ref
     // c'est la classe qui se carge d'ouvrir les serveurs
     // un serveur = une conversation
+
+    private int port_conv_generale = 6666;
+    private int port_conv_privee_1 = 7777;
+
+    public ArrayList<ServeurTCP> serveurs = new ArrayList<>();
+
+    private IProtocole protocole;
+
+    public Messagerie(){};
+
+    public void ouvrirMessagerie(){
+        serveurs.add(new ServeurTCP(this, new ProtocoleServeurGroupe(), port_conv_generale));
+        serveurs.add(new ServeurTCP(this, new ProtocoleServeurPrivee(), port_conv_privee_1));
+        for(ServeurTCP s : serveurs){
+            s.start();
+        }
+    }
+
+    //s'il faut rajouter des fonctions  : les déclarer dans l'interface !
 }
